7b5c30b12397cabcdaa8faa5a21174d09741e5ec
Joseph Caruana <jcaruana614@gmail.com>
2021-08-31 11:06:43 -0500

    build

---
+++ build/precache-manifest.22fb787399792b5d2a3e806f659d195c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac3f44a583c7621c69eaf265b9cd035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd71ceae9479561d5ef2",
+    "url": "/static/css/2.89fcc9a3.chunk.css"
+  },
+  {
+    "revision": "a15d9c62e1c98f5e9ac0",
+    "url": "/static/css/main.bed18094.chunk.css"
+  },
+  {
+    "revision": "bd71ceae9479561d5ef2",
+    "url": "/static/js/2.a57f2aa2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.a57f2aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15d9c62e1c98f5e9ac0",
+    "url": "/static/js/main.c28de394.chunk.js"
+  },
+  {
+    "revision": "99b1286ac86c23abbabb",
+    "url": "/static/js/runtime-main.6eba4eca.js"
+  },
+  {
+    "revision": "7b2773a8af954035722bb08448a68ac2",
+    "url": "/static/media/Spotbackb.7b2773a8.png"
+  },
+  {
+    "revision": "e2c805e89b683d5278827a1ac5879886",
+    "url": "/static/media/Spotbackw.e2c805e8.png"
+  },
+  {
+    "revision": "38b6737118f3cdc28faf4ae201e8b9f1",
+    "url": "/static/media/bg.38b67371.png"
+  },
+  {
+    "revision": "97d812a2b1ea835d95e3f3533516867f",
+    "url": "/static/media/c1.97d812a2.png"
+  },
+  {
+    "revision": "01c6d2ab35858785b2b94f9af8d45af6",
+    "url": "/static/media/c2.01c6d2ab.png"
+  },
+  {
+    "revision": "b67b8e6a1ce74cfe23dca7932bc1d48d",
+    "url": "/static/media/c3.b67b8e6a.png"
+  },
+  {
+    "revision": "f6b44075c9c397f848ea57b450d32f9f",
+    "url": "/static/media/c4.f6b44075.png"
+  },
+  {
+    "revision": "87ed5b6cd72a3a75fae48c19430fbad6",
+    "url": "/static/media/c5.87ed5b6c.png"
+  },
+  {
+    "revision": "6a6af63f38561144c7095a8e03956971",
+    "url": "/static/media/c6.6a6af63f.png"
+  },
+  {
+    "revision": "d82d9aa06c4b00052c364bb18c580c2d",
+    "url": "/static/media/dmitry.d82d9aa0.png"
+  },
+  {
+    "revision": "14c0f73f0bf274bb1f4a9b1c67f2c97c",
+    "url": "/static/media/dylan.14c0f73f.png"
+  },
+  {
+    "revision": "fd610eb66c05a14ff1b09ca3180f47f6",
+    "url": "/static/media/instagram.fd610eb6.png"
+  },
+  {
+    "revision": "d9e54baf88f88e9ef3aa0e4c8fecf624",
+    "url": "/static/media/iphoneX.d9e54baf.png"
+  },
+  {
+    "revision": "4e053a96ab158a8a3e7d3bc8d5f04fe8",
+    "url": "/static/media/joey.4e053a96.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "02488d09b2a5bf8c2e09e75db4eedb3f",
+    "url": "/static/media/twitter.02488d09.png"
+  }
+]);
